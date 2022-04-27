4c2a9831591bb961ea184a3c9b787b31304f3840
ThomGosselin <thomswim360@gmail.com>
2022-04-27 08:54:43 -0400

    Fix Menu animation

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-var app = function () {
-    var body = undefined;
-    var menu = undefined;
-    var menuItems = undefined;
-    var init = function init() {
-        body = document.querySelector('body');
-        menu = document.querySelector('.menu-icon');
-        menuItems = document.querySelectorAll('.nav__list-item');
-        applyListeners();
-    };
-    var applyListeners = function applyListeners() {
-        menu.addEventListener('click', function () {
-            return toggleClass(body, 'nav-active');
-        });
-    };
-    var toggleClass = function toggleClass(element, stringClass) {
-        if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
-    };
-    init();
-}();
-
 var cursor = $(".cursor"),
 		follower = $(".cursor-follower");
 
@@ -62,3 +41,24 @@ $(".link").on("mouseleave",function(){
 	cursor.removeClass("active");
 	follower.removeClass("active");
 });
+
+var app = function () {
+    var body = undefined;
+    var menu = undefined;
+    var menuItems = undefined;
+    var init = function init() {
+        body = document.querySelector('body');
+        menu = document.querySelector('.menu-icon');
+        menuItems = document.querySelectorAll('.nav__list-item');
+        applyListeners();
+    };
+    var applyListeners = function applyListeners() {
+        menu.addEventListener('click', function () {
+            return toggleClass(body, 'nav-active');
+        });
+    };
+    var toggleClass = function toggleClass(element, stringClass) {
+        if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+    };
+    init();
+}();
