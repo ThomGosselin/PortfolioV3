287858efc2d970804e65e02ba434d9244337d428
ThomGosselin <thomswim360@gmail.com>
2022-05-03 12:31:23 -0400

    Add DarkMode Btn V0.1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+//Section Dark - Light mode Button
+let container = document.querySelector(".containerDayNightBtn");
+let svg = document.getElementById("svg-animation");
+let icon = document.querySelector(".icon");
+
+icon.onclick = function () {
+  if (container.classList.contains("white")) {
+    svg.firstChild.remove();
+    changreBGToBlaack();
+  } else {
+    svg.firstChild.remove();
+    changreBGToWhith();
+  }
+};
+
+function changreBGToWhith() {
+  bodymovin.loadAnimation({
+    wrapper: document.getElementById("svg-animation"),
+    animType: "svg",
+    loop: false,
+    path: "https://raw.githubusercontent.com/Abdallah-Mohamed-Sayed/some-files/main/toSun",
+  });
+  setTimeout(() => {
+    container.classList.add("white");
+    container.classList.remove("black");
+  }, 276);
+}
+
+function changreBGToBlaack() {
+  bodymovin.loadAnimation({
+    wrapper: document.getElementById("svg-animation"),
+    animType: "svg",
+    loop: false,
+    path: "https://raw.githubusercontent.com/Abdallah-Mohamed-Sayed/some-files/main/toMoon",
+  });
+  setTimeout(() => {
+    container.classList.add("black");
+    container.classList.remove("white");
+  }, 276);
+}
+
+changreBGToWhith();
 var cursor = $(".cursor"),
 		follower = $(".cursor-follower"); 
 var posX = 0,
@@ -56,3 +98,4 @@ var app = function () {
     };
     init();
 }();
+
