1efa90b7f48baa7ee64ab266b31329bfda1c61f6
ThomGosselin <thomswim360@gmail.com>
2022-05-17 14:26:15 -0400

    Finished Contact Form + added file for sending the email

--- js/main.js
+++ js/main.js
@@ -19,10 +19,3 @@ var app = function () {
     init();
 }
 ();
-
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-  });
