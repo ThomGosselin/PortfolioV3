24352ebeb6994c79fe4c9c6c1c52f14e63ad52ba
ThomGosselin <thomswim360@gmail.com>
2022-05-18 12:44:54 -0400

    Added Base of the mail php file

---
+++ php/mail.php
@@ -0,0 +1,25 @@
+<?php
+    $name= $_POST['name'];
+    $email= $_POST['email'];
+    $message= $_POST['message'];
+
+    $email_from = 'alexiscegepmatane@gmail.com';
+
+    $email_subject = "Message du Portfolio";
+
+    $email_body = "Nom: $name.\n".
+                    "Email: $email.\n".
+                        "Message: $message.\n";
+
+    $to = "alexiscegepmatane@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+
+?>
