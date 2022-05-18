62d3ab617ee1d85cb70b724911e65b4f03e67b18
ThomGosselin <thomswim360@gmail.com>
2022-05-18 15:47:03 -0400

    Completed Photo viewer for CV in about me page

--- js/main.js
+++ js/main.js
@@ -21,9 +21,13 @@ var app = function () {
 ();
 
 let buttonCV = document.querySelector(".buttonCV");
-let clip = document.querySelector(".clip");
+let CV = document.querySelector(".CV");
+let close = document.querySelector(".close");
 buttonCV.onclick = function(){
     buttonCV.classList.add('active');
-    clip.classList.add('active');
-
+    CV.classList.add('active');
+}
+close.onclick = function(){
+    buttonCV.classList.remove('active');
+    CV.classList.remove('active');
 }
